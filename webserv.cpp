24fd2161e04bfe4fbb64773a5bd5d7d014683b4f
hey-chocopie <ghdyd6372@naver.com>
2022-03-26 15:21:04 +0900

    수정 : segment error 난 부분 수정

--- webserv.cpp
+++ webserv.cpp
@@ -16,36 +16,36 @@
 //	}
 //}
 
-bool connect_socket_and_parsing(IO_fd_set &fds, connection &cn, struct timeval &timeout, int &fd_num, request &rq)
+bool connect_socket_and_parsing(IO_fd_set *fds, connection *cn, struct timeval *timeout, int *fd_num, request *rq)
 {
-	fds.cpy_read_fds = fds.read_fds;
-	fds.cpy_write_fds = fds.write_fds;
-	timeout.tv_sec = 1; //1초로 타임 제한
-	timeout.tv_usec = 0;
-	if ((fd_num = select(cn.max_fd() + 1, &(fds.cpy_read_fds), &(fds.cpy_write_fds), 0, &timeout)) == -1)
+	(*fds).cpy_read_fds = (*fds).read_fds;
+	(*fds).cpy_write_fds = (*fds).write_fds;
+	(*timeout).tv_sec = 1; //1초로 타임 제한
+	(*timeout).tv_usec = 0;
+	if (((*fd_num) = select((*cn).max_fd() + 1, &((*fds).cpy_read_fds), &((*fds).cpy_write_fds), 0, timeout)) == -1)
 		throw (select_error());
-	else if (!fd_num)
+	else if (!(*fd_num))
 		return true;
-	for (connection::iterator it = cn.fd_arr_begin(); it != cn.fd_arr_end(); ++it)
+	for (connection::iterator it = (*cn).fd_arr_begin(); it != (*cn).fd_arr_end(); ++it)
 	{	
-		if (FD_ISSET(it->fd, &(fds.cpy_read_fds)))
+		if (FD_ISSET(it->fd, &((*fds).cpy_read_fds)))
 		{
-			if (cn.is_server_socket(it->fd))
+			if ((*cn).is_server_socket(it->fd))
 			{
-				cn.connect_client(it->fd);
+				(*cn).connect_client(it->fd);
 				break ;
 			}
 			else
-				cn.get_client_msg(it->fd, rq);
+				(*cn).get_client_msg(it->fd, (*rq));
 		}
-		if (FD_ISSET(it->fd, &(fds.cpy_write_fds)))
+		if (FD_ISSET(it->fd, &((*fds).cpy_write_fds)))
 		{
 			/*std::string	rp = "print done!\n";
 			send(1, rp.c_str(), strlen(rp.c_str()), 0);*/ //여기선 단순히 문자열 출력하는 방법
 			std::string	rp = "HTTP/1.1 200 OK\r\n\r\n<h1>TESTING</h1>";
 			send(it->fd, rp.c_str(), strlen(rp.c_str()), 0); //recv에 맞춰서 write도 send로 변경
-			FD_CLR(it->fd, &(fds.write_fds));
-			cn.disconnect_client(it->fd); //출력까지 하고 나서 cn 내부에 아직 남아있는 해당 fd 정보를 삭제
+			FD_CLR(it->fd, &((*fds).write_fds));
+			(*cn).disconnect_client(it->fd); //출력까지 하고 나서 cn 내부에 아직 남아있는 해당 fd 정보를 삭제
 		}
 	}
 	return false;
@@ -61,7 +61,7 @@ void	exec_webserv(config &cf)
 
 	while (true)
 	{
-		if(connect_socket_and_parsing(fds, cn, timeout, fd_num, rq) == true)
+		if(connect_socket_and_parsing(&fds, &cn, &timeout, &fd_num, &rq) == true)
 			continue;
 		exec_request(cf, fds.write_fds, rq);
 	}
